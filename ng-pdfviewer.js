f52586fc8b6d65efd7fee2fb4c71b330db8954a5
Ge Yang <yangge1987@gmail.com>
2015-01-10 12:00:18 -0600

    fix multi page bug where if number of canvases is larger than number of pages in the document, renderInProgress never reset

--- ng-pdfviewer.js
+++ ng-pdfviewer.js
@@ -7,50 +7,84 @@
  */
 
 angular.module('ngPDFViewer', []).
-directive('pdfviewer', [ '$parse', function($parse) {
-	var canvas = null;
+directive('pdfviewer', [ '$log', '$q', function($log, $q) {
+    var _pageToShow = 1;
+	var canvases = [];
 	var instance_id = null;
 
 	return {
 		restrict: "E",
-		template: '<canvas></canvas>',
+		template: '',
 		scope: {
 			onPageLoad: '&',
 			loadProgress: '&',
 			src: '@',
 			id: '=',
-			pdfUri: '=pdfUri'
+			pdfUri: '=pdfUri',
+			pagesToShow: '@',
+			scale: '@'
 		},
 		controller: [ '$scope', function($scope) {
 			$scope.pageNum = 1;
 			$scope.pdfDoc = null;
-			$scope.scale = 1.0;
+			$scope.renderInProgress = false;
+			$scope.forceReRender = true;
 
 			$scope.documentProgress = function(progressData) {
 				if ($scope.loadProgress) {
 					$scope.loadProgress({state: "loading", loaded: progressData.loaded, total: progressData.total});
 				}
 			};
-
-			$scope.loadPDF = function(path) {
-				console.log('loadPDF ', path);
-				PDFJS.getDocument(path, null, null, $scope.documentProgress).then(function(_pdfDoc) {
-					$scope.pdfDoc = _pdfDoc;
-					$scope.renderPage($scope.pageNum, function(success) {
-						if ($scope.loadProgress) {
-							$scope.loadProgress({state: "finished", loaded: 0, total: 0});
-						}
+			$scope.setScale = function (newValue) {
+				if (angular.isNumber(newValue)) {
+					$scope.scale = newValue || 1;
+					$scope.forceReRender = true;
+				}
+			};
+			$scope.renderDocument = function () {
+				$log.debug("Render Document");
+				angular.forEach(canvases, function (canvas, index) {
+					if (index == 0)
+						$scope.renderInProgress = true;
+					var pageNumber = index + $scope.pageNum;
+					$scope.renderPage(pageNumber, canvas, function (success) {
+						$log.debug("Rendered Page <" + pageNumber + "> SUCCESS <" + success + ">");
+                        $scope.renderInProgress = false;
+                        $scope.forceReRender = false;
 					});
+				});
+			};
+			$scope.loadPDF = function(path) {
+				$log.debug('loadPDF <', path + '>');
+
+				var deferred = $q.defer();
+				PDFJS.getDocument(path, null, null, $scope.documentProgress).then(function(_pdfDoc) {
+					$log.debug('Document read');
+					$scope.pdfDoc = _pdfDoc;
+                    if ($scope.loadProgress) {
+                        $scope.loadProgress({state: "finished", loaded: 0, total: 0});
+                    }
+					deferred.resolve($scope.pdfDoc);
 				}, function(message, exception) {
-					console.log("PDF load error: " + message);
+					$log.debug("PDF load error: " + message + " < " + exception + " > ");
+					deferred.reject(message);
 					if ($scope.loadProgress) {
 						$scope.loadProgress({state: "error", loaded: 0, total: 0});
 					}
 				});
+				return deferred.promise;
 			};
 
-			$scope.renderPage = function(num, callback) {
-				console.log('renderPage ', num);
+			$scope.renderPage = function(num, canvas, callback) {
+				$log.debug('renderPage #' + num);
+				var renderedPageInCanvas = canvas.getAttribute("rendered");
+                if (renderedPageInCanvas === num && !$scope.forceReRender) {
+                    $log.debug("Skipping page <"+num+">");
+                    if (callback) {
+                        callback(true);
+                    }
+                    return;
+                }
 				$scope.pdfDoc.getPage(num).then(function(page) {
 					var viewport = page.getViewport($scope.scale);
 					var ctx = canvas.getContext('2d');
@@ -58,8 +92,9 @@ directive('pdfviewer', [ '$parse', function($parse) {
 					canvas.height = viewport.height;
 					canvas.width = viewport.width;
 
-					page.render({ canvasContext: ctx, viewport: viewport }).promise.then(
-						function() { 
+					page.render({ canvasContext: ctx, viewport: viewport }).then(
+						function() {
+							canvas.setAttribute("rendered", num);
 							if (callback) {
 								callback(true);
 							}
@@ -71,11 +106,20 @@ directive('pdfviewer', [ '$parse', function($parse) {
 							if (callback) {
 								callback(false);
 							}
-							console.log('page.render failed');
+							$log.debug('page.render failed');
 						}
 					);
 				});
 			};
+
+			$scope.$on('pdfviewer.setScale', function(evt, id, scale) {
+				if (id !== instance_id) {
+					return;
+				}
+
+				$scope.setScale(scale);
+				$scope.renderDocument();
+			});
 
 			$scope.$on('pdfviewer.nextPage', function(evt, id) {
 				if (id !== instance_id) {
@@ -84,7 +128,7 @@ directive('pdfviewer', [ '$parse', function($parse) {
 
 				if ($scope.pageNum < $scope.pdfDoc.numPages) {
 					$scope.pageNum++;
-					$scope.renderPage($scope.pageNum);
+					$scope.renderDocument();
 				}
 			});
 
@@ -95,7 +139,7 @@ directive('pdfviewer', [ '$parse', function($parse) {
 
 				if ($scope.pageNum > 1) {
 					$scope.pageNum--;
-					$scope.renderPage($scope.pageNum);
+					$scope.renderDocument();
 				}
 			});
 
@@ -106,27 +150,80 @@ directive('pdfviewer', [ '$parse', function($parse) {
 
 				if (page >= 1 && page <= $scope.pdfDoc.numPages) {
 					$scope.pageNum = page;
-					$scope.renderPage($scope.pageNum);
+					$scope.renderDocument();
 				}
 			});
 		} ],
 		link: function(scope, iElement, iAttr) {
-			canvas = iElement.find('canvas')[0];
-			instance_id = iAttr.id;
+			var instance_id = iAttr.id;
+			var createCanvas = function(iElement, count){
+				canvases = iElement.find('canvas');
 
+				if (canvases.length > count) {
+					//I need to remove canvases
+					for (var i = count; i < canvases.length; i++) {
+						angular.element(canvases[i]).remove();
+					}
+				} else {
+					//I need to add more canvas
+					for (var i = canvases.length; i < count; i++) {
+						var tmpCanvas = angular.element('<canvas>');
+						tmpCanvas[0].setAttribute("id", "page" + (i + 1));
+						iElement.append(tmpCanvas);
+					}
+				}
+				canvases = iElement.find('canvas');
+			};
+            var openDocCallback = function (pdfDoc){
+                $log.debug('PDF Loaded');
+                scope.pagesToShow = scope.pagesToShow == 0 ? scope.pdfDoc.numPages : scope.pagesToShow;
+				console.log(scope.pagesToShow);
+                createCanvas(iElement, scope.pagesToShow);
+                scope.renderDocument();
+            };
 			iAttr.$observe('src', function(v) {
-				console.log('src attribute changed, new value is', v);
+				$log.debug('src attribute changed, new value is <' + v + ">");
 				if (v !== undefined && v !== null && v !== '') {
 					scope.pageNum = 1;
-					scope.loadPDF(scope.src);
+					scope.loadPDF(scope.src).then(openDocCallback, function(meg){
+                        $log.debug(meg);
+					});
 				}
 			});
 			scope.$watch('pdfUri', function (newVal, oldVal){
 				/* somehow initial load always have oldVal updated. */
-				//if (newVal === oldVal) return ;
+				if (!newVal && !oldVal || newVal === oldVal) return;
 				scope.pageNum = 1;
-				scope.loadPDF(scope.pdfUri);
-			})
+				$log.debug(scope.pdfUri.toString());
+				scope.loadPDF(scope.pdfUri).then(openDocCallback, function(meg){
+					$log.debug(meg);
+				});
+			});
+
+			iAttr.$observe('pagesToShow', function (v) {
+				//SKIP if rendering is in progress or document not loaded
+				$log.debug('observerd pages to show change <' + v + '>');
+				$log.debug('renderInProgress: <' + scope.renderInProgress + '>');
+				$log.debug('pagesToShow is number? : <' + angular.isNumber(parseInt(v)) + '>');
+				if (scope.pdfDoc == null || scope.renderInProgress || !angular.isNumber(parseInt(v))) {
+					scope.pagesToShow = _pageToShow;
+					return;
+				}
+				$log.debug('pages-to-show attribute changed, new value is <' + v + ">");
+				console.log(scope.pdfDoc);
+				scope.pagesToShow = scope.pagesToShow == 0 ? scope.pdfDoc.numPages : scope.pagesToShow;
+				createCanvas(iElement, scope.pagesToShow);
+				scope.renderDocument();
+			});
+			iAttr.$observe('scale', function (v) {
+				//SKIP if rendering is in progress or document not loaded
+				$log.debug('observerd scale change <' + v + '>');
+				if (scope.pdfDoc == null || scope.renderInProgress || !angular.isNumber(parseInt(v)))
+					return;
+				$scope.forceReRender = true;
+				$log.debug('scale attribute changed, new value is <' + v + ">");
+				scope.renderDocument();
+			});
 		}
 	};
 }]).
@@ -153,6 +250,9 @@ service("PDFViewerService", [ '$rootScope', function($rootScope) {
 			},
 			gotoPage: function(page) {
 				$rootScope.$broadcast('pdfviewer.gotoPage', instance_id, page);
+			},
+			setScale: function (scale) {
+				$rootScope.$broadcast('pdfviewer.setScale', instance_id, scale);
 			}
 		};
 	};
